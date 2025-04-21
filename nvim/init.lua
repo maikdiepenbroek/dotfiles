725cb46008a66e82ed03dbc461e5470e8841bde5
Maik Diepenbroek <m.diepenbroek@gmail.com>
2025-04-21 13:46:47 +0200

    small formatting in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -39,9 +39,11 @@ vim.opt.cursorline = true
 
 vim.opt.scrolloff = 20
 
-vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.opt.tabstop = 4
+
+vim.opt.hlsearch = true
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
